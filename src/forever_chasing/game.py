58a3a88bbafcf6ab7281b0c65280d7d2275fe595
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-10-24 17:29:15 +0300

    Add borders and collision detection

--- src/forever_chasing/game.py
+++ src/forever_chasing/game.py
@@ -7,6 +7,7 @@ ACCELERATION = 0.3
 MASS = 100
 RESISTANCE_COEF = 5
 TICK_MS = 50
+MAP_SIDE = 600
 
 
 class MoveDirection(enum.Enum):
@@ -21,12 +22,13 @@ class Circle:
         self,
         canvas: tk.Canvas,
         init_coord=(0, 0),
-        size=10,
+        size=20,
         fill_color="black",
     ):
         self.canvas = canvas
         self.acc_direction = np.array([0, 0])
         self.speed = np.array([0, 0])
+        self.size = size
         self.circle = self.canvas.create_oval(
             1, 1, size, size, fill=fill_color
         )  # or zeros in first two coordinates?
@@ -54,8 +56,24 @@ class Circle:
     def move(self):
         self.speed = self.make_step(self.acc_direction, self.speed)
         self.speed = self.cap_speed(self.speed)
+        self.impose_borders()  # so many side effects it hurts mind
         self.canvas.move(self.circle, *self.speed)
         self.canvas.after(TICK_MS, self.move)
+
+    def impose_borders(self):
+        coords = self.get_coord()
+        if not 0 <= coords[0] <= MAP_SIDE:
+            self.speed[0] *= -1
+            move_x = MAP_SIDE - coords[0] if self.speed[0] < 0 else 0 - coords[0]
+            self.canvas.move(self.circle, move_x, 0)
+
+        if not 0 <= coords[1] <= MAP_SIDE:
+            self.speed[1] *= -1
+            move_y = MAP_SIDE - coords[1] if self.speed[1] < 0 else 0 - coords[1]
+            self.canvas.move(self.circle, 0, move_y)
+        coords = self.get_coord()
+        assert 0 <= coords[0] <= MAP_SIDE, f"wrong x: {coords[0]}"
+        assert 0 <= coords[1] <= MAP_SIDE, f"wrong y: {coords[1]}"
 
     def change_acceleration_direction(self, direction) -> None:
         self.acc_direction = np.array(direction.value)
@@ -73,6 +91,14 @@ class Circle:
 
 def is_collision_circles(circle1, circle2) -> bool:
     """Should appreciate borders of objects"""
+    c1, c2 = circle1.get_coord(), circle2.get_coord()
+    d1, d2 = circle1.size, circle2.size
+    combined_radius = (d1 + d2) / 2
+    coord_diff = np.linalg.norm(c1 - c2)
+    if coord_diff < combined_radius:  # imprecise
+        circle1.canvas.configure(bg="blue")  # unbelievable side effect
+        return True
+    circle1.canvas.configure(bg="black")
     return False
 
 
@@ -94,12 +120,13 @@ class CircleBot(Circle):
 
     def move(self):
         self.acc_direction = direction_to_target(self, self.target)
+        is_collision_circles(self, self.target)
         super().move()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    canvas = tk.Canvas(root, bg="black", width=400, height=400)
+    canvas = tk.Canvas(root, bg="black", width=MAP_SIDE, height=MAP_SIDE)
     canvas.pack(fill="both", expand=True)
 
     cvs = Circle(canvas, init_coord=(200, 200))
